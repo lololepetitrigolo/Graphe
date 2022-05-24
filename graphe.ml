e53fc94a5e8722ca6e1b26c1c46c0cba2b938816
lololepetitrigolo <62268724+lololepetitrigolo@users.noreply.github.com>
2022-05-24 14:42:55 +0200

    Add files via upload

--- graphe.ml
+++ graphe.ml
@@ -1,3 +1,5 @@
+open Tas
+
 type graphe = {mat:int array array; sommet:char array;mutable n:int};;
 
 let create_graphe(len:int):graphe=
@@ -23,6 +25,34 @@ let trouve_ind(a:char)(g:graphe):int =
 
 let ajout_arc(a:char)(b:char)(w:int)(g:graphe):unit = g.mat.(trouve_ind a g).(trouve_ind b g) <- w;;
 
+let init_dist(d:char)(g:graphe):int array=
+  let dist = Array.make g.n max_int in
+  dist.(trouve_ind d g) <- 0;
+  dist
+;;
+
+let dijkstra(d:char)(a:char)(g:graphe):int=
+  let dist = init_dist d g in
+  let q = create_heap g.n in
+  let u = ref (0,0);
+  let alt = ref 0;
+  for i=0 to g.n -1 do
+    add((dist.(i),i),q);
+  done;
+  while not(is_empty q) do
+    u := snd(extract q);
+    for i=0 to g.n do
+      if g.mat.(!u).(i) <> 0 then begin
+          alt := dist.(!u) + g.mat.(!u).(i);
+          if !alt < dist.(i) then begin
+              dist.(i) <- !alt;
+              add((!alt,i),q);
+          end;
+      end;
+    done;
+  done;
+  dist.(trouve_ind a g)
+;;
 let g = create_graphe 4;;
 ajout_sommet 'A' g;;
 ajout_sommet 'B' g;;
